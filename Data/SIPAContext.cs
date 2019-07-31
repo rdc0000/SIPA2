717482c11dc0a137359c4e4b71dd7694c07c04fa
rdc0000 <rdc.159@hotmail.com>
2019-07-31 08:45:51 -0500

    Initial commit

---
+++ Data/SIPAContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SIPA.Models;
+
+    public class SIPAContext : DbContext
+    {
+        public SIPAContext (DbContextOptions<SIPAContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SIPA.Models.Autoservicio> Autoservicio { get; set; }
+    }
